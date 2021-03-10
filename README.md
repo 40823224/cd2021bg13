78ebc8a5ce0f7219b17056949d876aee14c558bc
40823224 <40823224@gm.nfu.edu.tw>
2021-03-10 16:30:55 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cmstemplate
+https://40823224.github.io/cd2021bg13/content/index.html
--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '\n \n', 'tags': '', 'url': 'About.html'}, {'title': '筆記', 'text': '', 'tags': '', 'url': '筆記.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '組員： \n 40823223\xa0 倉儲 ／ 網頁 \n 40823224\xa0 倉儲 ／ 網頁 \n \n', 'tags': '', 'url': 'About.html'}, {'title': '筆記', 'text': '', 'tags': '', 'url': '筆記.html'}]};
