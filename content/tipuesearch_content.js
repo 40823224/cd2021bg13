af28cf37a1fe8951513a55278d2852f4be4a6d81
40823224 <40823224@gm.nfu.edu.tw>
2021-03-12 00:45:46 +0800

    add

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '組員： \n 40823223\xa0 倉儲 ／ 網頁 \n 40823224\xa0 倉儲 ／ 網頁 \n \n', 'tags': '', 'url': 'About.html'}, {'title': '筆記', 'text': '', 'tags': '', 'url': '筆記.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '組員： \n 40823223\xa0 倉儲 ／ 網頁 \n 40823224\xa0 倉儲 ／ 網頁 \n \n', 'tags': '', 'url': 'About.html'}, {'title': '筆記', 'text': 'clone整個倉儲 \n git clone --recurse-submodule https://github.com/40823224/cd2021bg13.git \n', 'tags': '', 'url': '筆記.html'}]};
